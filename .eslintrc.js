7997aceb45eb24a0ad5e43ec4e8e65ea8bc18904
keroming <keroming@tencent.com>
2021-01-11 04:47:35 +0800

    feat: 完善插件，丰富插件选项

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,25 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true,
     mocha: true,
+    es6: true
   },
-  extends: [
-    'airbnb'
-    'plugin:prettier/recommended',
-  ],
-}
+  parser: 'babel-eslint',
+  extends: ['airbnb-base/legacy'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  rules: {
+    'no-underscore-dangle': ['off'],
+    'no-param-reassign': ['off'],
+    'consistent-return': ['off'],
+    'max-len': ['off'],
+    'no-console': ['off']
+  }
+};
--- README.md
+++ README.md
@@ -16,7 +16,17 @@
 
 -----
 
-xxx
+本插件有以下功能：
+- 快速分析 webpack 项目文件依赖，并生成依赖表
+    - 依赖表类型：树形结构、扁平化、反向扁平化
+    - 根据 webpack config context，输出 绝对 / 相对路径
+-  自动分析 Git 修改文件所引起的入口文件变化
+    - 查找影响类型：入口文件、符合 `minimatch` 规则匹配的文件
+    - 根据 webpack config context，输出 绝对 / 相对路径
+
+**注意：**这里仅分析项目文件，所以项目依赖 `node_modules` 里面的模块会被忽略
+
+
 
 ## usage
 
@@ -29,4 +39,4 @@ npm install fast-dependencies-analyzer-webpack-plugin --save-dev
 
 **use**
 
-xxxx
+请参阅 [lib/index.js](lib/index.js) `this.options` 注释（暂时没时间详细写明，看一下注释吧）
